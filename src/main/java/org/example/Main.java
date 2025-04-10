14332463aea74ce1b8086e0ccbf3c0d9ebf03e90
Eric Schmidt <eric0802@web.de>
2025-04-10 10:28:23 +0200

    add record Teacher

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,9 @@ public class Main {
         Student student2 = new Student("S2", "Erika Musterfrau", "Beispielweg 2", 2);
         Student student3 = new Student("S3", "John Doe", "Main Street 3", 1);
 
+        // Create teachers
+        Teacher mathTeacher = new Teacher("T1", "Dr. Schmidt", "Mathematics");
+        Teacher physicsTeacher = new Teacher("T2", "Prof. Müller", "Physics");
+
     }
 }
